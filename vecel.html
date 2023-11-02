6d120e62423eb495bf91b6c41a7a2791f0844092
Jahirul103 <149692892+Jahirul103@users.noreply.github.com>
2023-11-02 20:28:26 +0600

    Add files via upload

---
+++ vecel.html
@@ -0,0 +1,7 @@
+<form action="/generate-chatbot-link" method="post">
+    <input type="text" name="prompt" placeholder="Enter your prompt">
+    <input type="text" name="business_data" placeholder="Enter your business data">
+    <input type="submit" value="Generate Chatbot Link">
+</form>
+
+<div id="chatbot-link"></div>
---
+++ vecel.py
@@ -0,0 +1,31 @@
+import json
+import requests
+
+from openai import OpenAIApi
+
+class BackendApplication:
+    def init(self, database):
+        self.database = database
+
+        # Create an OpenAI API object
+        self.openai = OpenAIApi(openai_api_key="YOUR_OPENAI_API_KEY")
+
+    def generate_chatbot_link(self, prompt, business_data):
+        # Extract the prompt and business data from the database
+        prompt = self.database.get_prompt()
+        business_data = self.database.get_business_data()
+
+        # Generate a ChatGPT prompt based on the extracted data
+        chatgpt_prompt = f"Generate a chatbot based on the following prompt: {prompt} and business data: {business_data}"
+
+        # Send the ChatGPT prompt to the ChatGPT API and receive the response
+        chatgpt_response = self.openai.generate(chatgpt_prompt)
+
+        # Save the ChatGPT response to the database
+        self.database.save_chatbot_response(chatgpt_response)
+
+        # Generate a link to the chatbot based on the saved response
+        chatbot_link = f"https://your-website.com/chatbot/{chatgpt_response.id}"
+
+        # Return the chatbot link
+        return chatbot_link
